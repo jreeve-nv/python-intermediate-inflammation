d8f374c750af1baa9affa417624904f7d2ec5be1
jreeve-nv <jennifer.reeve@netvalue.nz>
2024-08-29 14:10:39 +1200

    data source classes for csv and json

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -7,18 +7,41 @@ import numpy as np
 
 from inflammation import models, views
 
+class CSVDataSource:
+    """
+    Loads all the CSV files within a specified directory
+    """
+    def __init__(self, path):
+        self.path = path
 
-def analyse_data(data_dir):
+    def load_data(self):
+        data_file_paths = glob.glob(os.path.join(self.path, 'inflammation*.csv'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No CSV files found in path {self.path}")
+        data = map(models.load_csv, data_file_paths)
+        return list(data)
+    
+class JSONDataSource:
+    """
+    Loads all the JSON files within a specified directory
+    """
+    def __init__(self, path):
+        self.path = path
+
+    def load_data(self):
+        data_file_paths = glob.glob(os.path.join(self.path, 'inflammation*.json'))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No JSON files found in path {self.path}")
+        data = map(models.load_json, data_file_paths)
+        return list(data)
+
+def analyse_data(data_source):
     """Calculates the standard deviation by day between datasets.
 
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
-
+    data = data_source.load_data()
 
     means_by_day = map(models.daily_mean, data)
     means_by_day_matrix = np.stack(list(means_by_day))
